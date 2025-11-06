1f7381a8e4fb3143402daf801febd79e76c2d099
jeonjunhee93 <lllllljz@naver.com>
2025-11-07 04:18:03 +0900

    Add files via upload

---
+++ LifeRPG_FinalBuild (1)/src/LifeRPG.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LifeRPG = () => {
+  return (
+    <div>
+      <h1>Life R.P.G</h1>
+      <p>게임 기능은 여기에 추가됩니다.</p>
+    </div>
+  );
+};
+
+export default LifeRPG;
